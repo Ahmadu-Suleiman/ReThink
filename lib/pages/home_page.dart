a3c0b67824e50cce6af6b5f9d955105bbd6c101c
Ahmadu-Suleiman <ahmadumeta4.1@gmail.com>
2024-09-26 09:09:17 +0100

    add bottom nav bar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  PersistentTabController controller = PersistentTabController(initialIndex: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('ReThink',
+                style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold))),
+        body: PersistentTabView(context,
+            controller: controller,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            screens: [
+              Container(),
+              Container(),
+              Container(),
+              Container()
+            ],
+            items: [
+              PersistentBottomNavBarItem(
+                  activeColorPrimary: Theme.of(context).colorScheme.primary,
+                  inactiveColorPrimary: Theme.of(context).colorScheme.secondary,
+                  icon: const Icon(Icons.home),
+                  title: 'Home'),
+              PersistentBottomNavBarItem(
+                  activeColorPrimary: Theme.of(context).colorScheme.primary,
+                  inactiveColorPrimary: Theme.of(context).colorScheme.secondary,
+                  icon: const Icon(Icons.eco),
+                  title: 'Profile'),
+              PersistentBottomNavBarItem(
+                  activeColorPrimary: Theme.of(context).colorScheme.primary,
+                  inactiveColorPrimary: Theme.of(context).colorScheme.secondary,
+                  icon: const Icon(Icons.school),
+                  title: 'Learn'),
+              PersistentBottomNavBarItem(
+                  activeColorPrimary: Theme.of(context).colorScheme.primary,
+                  inactiveColorPrimary: Theme.of(context).colorScheme.secondary,
+                  icon: const Icon(Icons.event),
+                  title: 'Challenge')
+            ]),
+        floatingActionButton: FloatingActionButton(
+            shape: const CircleBorder(),
+            onPressed: () {},
+            child: const Icon(Icons.photo_camera_back)),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterFloat);
+  }
+}
