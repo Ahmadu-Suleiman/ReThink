ad94d0c1d83df51c129aec09e3425bb931ff1869
Ahmadu-Suleiman <ahmadumeta4.1@gmail.com>
2024-09-27 01:11:16 +0100

    use memory list for challenges

--- lib/pages/challenge_page.dart
+++ lib/pages/challenge_page.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 
-class ChallengePage extends StatelessWidget {
-  final List<String> dailyChallenges = [
-    'Learn a new programming language',
-    'Practice coding problems',
-    'Read a book related to technology',
-    'Exercise for 30 minutes',
-    'Meditate for 15 minutes',
-    'Call a friend or family member',
-    'Try a new recipe',
-    'Declutter a small area of your room',
-    'Learn a new dance move',
-    'Practice gratitude journaling',
-  ];
+import '../util.dart';
 
+class ChallengePage extends StatelessWidget {
   final List<DateTime> challengeDates = [
     DateTime.now().add(const Duration(days: 0)),
     DateTime.now().add(const Duration(days: 1)),
@@ -33,16 +22,14 @@ class ChallengePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: dailyChallenges.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(child: Text('${challengeDates[index].day}')),
-            title: Text(dailyChallenges[index]),
-            subtitle: Text('${challengeDates[index].month}/${challengeDates[index].day}'),
-          );
-        },
-      ),
-    );
+        body: ListView.builder(
+            itemCount: Util.dailyChallenges.length,
+            itemBuilder: (context, index) {
+              DateTime date = DateTime.now().add(Duration(days: index));
+              return ListTile(
+                  leading: CircleAvatar(child: Text('${date.day}')),
+                  title: Text(Util.dailyChallenges[index]),
+                  subtitle: Text('${date.month}/${date.day}'));
+            }));
   }
 }
